2b774ca4180adf5427498300cde2a9b64d747680
Daniel Olaszy <daniel.olaszy@gmail.com>
2021-07-29 11:03:40 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-
+// import logo from './logo.svg';
+import React, { useState } from "react";
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
+  const [shitf, setShift] = useState(0);
+  const [cypherText, setCypherText] = useState("");
+  const [plainText, setPlainText] = useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <header className="App-header">
+      <div className="row">
+        <h1>Caesar Cipher</h1>
+      </div>
+      <div className="row">
+        <div className="col">
+          <button className="btn btn-primary">-</button>
+        </div>
+        <div className="col">
+          <p>{shitf}</p>
+        </div>
+        <div className="col">
+          <button className="btn btn-primary">+</button>
+        </div>
+      </div>
+    </header>
   );
 }
 
