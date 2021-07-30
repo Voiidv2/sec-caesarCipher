5f631d60fef37bfac8f94d506ccbdecf4d1e7000
Daniel Olaszy <daniel.olaszy@gmail.com>
2021-07-30 14:04:10 +0100

    Added functions to shift the alphabet

--- src/App.js
+++ src/App.js
@@ -1,20 +1,67 @@
 // import logo from './logo.svg';
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
+  const [alphabet, setAlphabet] = useState([
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ]);
+
   const [shift, setShift] = useState(0);
   const [cypherText, setCypherText] = useState("");
   const [plainText, setPlainText] = useState("");
 
   const handleShiftPlus = (e) => {
     e.preventDefault();
-    setShift(shift + 1);
+    if (shift < 26) {
+      console.log("Shifting...");
+      const target = alphabet[alphabet.length - 1];
+      alphabet.pop();
+      alphabet.unshift(target);
+      console.log(alphabet);
+      setShift(shift + 1);
+    } else {
+      console.log("Cannot go past 26");
+    }
   };
 
   const handleShiftMinus = (e) => {
     e.preventDefault();
-    setShift(shift - 1);
+    if (shift > -26) {
+      console.log("Shifting...");
+      const target = alphabet[0];
+      alphabet.shift();
+      alphabet.push(target);
+      console.log(alphabet);
+      setShift(shift - 1);
+    } else {
+      console.log("Cannot go past -26");
+    }
   };
 
   return (
@@ -26,26 +73,26 @@ function App() {
       </section>
       <section className="row">
         <div className="col-6 mx-auto">
-          <label for="plainText" class="form-label">
+          <label htmlFor="plainText" className="form-label">
             Plain Text
           </label>
-          <textarea class="form-control" id="plainText" rows="4"></textarea>
+          <textarea className="form-control" id="plainText" rows="4"></textarea>
         </div>
       </section>
 
       <section className="col-6 mx-auto my-5">
         <div className="d-grid gap-2">
           <section className="row justify-content-center ">
-            <div class="col text-end my-auto">
-              <button class="btn btn-primary px-4" type="button" onClick={handleShiftMinus}>
+            <div className="col text-end my-auto">
+              <button className="btn btn-primary px-4" type="button" onClick={handleShiftMinus}>
                 -
               </button>
             </div>
-            <div class="col-2 text-center">
+            <div className="col-2 text-center">
               <h3>{shift}</h3>
             </div>
-            <div class="col text-start my-auto">
-              <button class="btn btn-primary px-4" type="button" onClick={handleShiftPlus}>
+            <div className="col text-start my-auto">
+              <button className="btn btn-primary px-4" type="button" onClick={handleShiftPlus}>
                 +
               </button>
             </div>
@@ -57,10 +104,10 @@ function App() {
 
       <section className="row">
         <div className="col-6 mx-auto p-2">
-          <label for="cypherText" class="form-label">
-            Cypher Text
+          <label htmlFor="cypherText" className="form-label">
+            Cipher Text
           </label>
-          <textarea class="form-control" id="cypherText" rows="4"></textarea>
+          <textarea className="form-control" id="cypherText" rows="4"></textarea>
         </div>
       </section>
     </header>
